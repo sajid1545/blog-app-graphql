77907957639b2a865d481d5b809a9d1405e1d4f4
Sajjad Abdullah <clashking1545@gmail.com>
2025-05-16 02:30:38 +0600

    Refactors Apollo Client initialization
    Simplifies Apollo Client initialization by directly passing the URI and headers to the constructor.
    
    This removes the need for separate HttpLink and authLink configurations and streamlines the authentication process by directly accessing the token from local storage within the client's header configuration.

--- graphql-frontend/src/utils/apolloClient.js
+++ graphql-frontend/src/utils/apolloClient.js
@@ -1,35 +1,47 @@
-// apolloClient.js
-import {
-  ApolloClient,
-  ApolloLink,
-  HttpLink,
-  InMemoryCache,
-} from "@apollo/client";
+// // apolloClient.js
+// import {
+//   ApolloClient,
+//   ApolloLink,
+//   HttpLink,
+//   InMemoryCache,
+// } from "@apollo/client";
 
-const getToken = () => {
-  return localStorage.getItem("token"); // or pull from context if necessary
-};
+// const getToken = () => {
+//   return localStorage.getItem("token"); // or pull from context if necessary
+// };
 
-const authLink = new ApolloLink((operation, forward) => {
-  const token = getToken();
+// const authLink = new ApolloLink((operation, forward) => {
+//   const token = getToken();
 
-  operation.setContext(({ headers = {} }) => ({
-    headers: {
-      ...headers,
-      Authorization: token || "", // 👈 No "Bearer" prefix
-    },
-  }));
+//   operation.setContext(({ headers = {} }) => ({
+//     headers: {
+//       ...headers,
+//       Authorization: token || "", // 👈 No "Bearer" prefix
+//     },
+//   }));
 
-  return forward(operation);
-});
+//   return forward(operation);
+// });
 
-const httpLink = new HttpLink({
-  uri: "http://localhost:4000/",
-});
+// const httpLink = new HttpLink({
+//   uri: "http://localhost:4000/",
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+// export default client;
+
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
+  headers: {
+    authorization: localStorage.getItem("token"),
+  },
 });
 
 export default client;
